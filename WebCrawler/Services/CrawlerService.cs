7bf56ce815202336c217cdba2cc7b2ef98469f1f
Bruno <bruno.rp45@gmail.com>
2025-02-19 11:27:27 -0300

    Adicionar CrawlerService para extração de proxies
    Implementa a classe `CrawlerService` que utiliza `HttpClient` para baixar conteúdo HTML de URLs. Adiciona métodos assíncronos para extrair proxies e garantir que as respostas HTTP sejam bem-sucedidas antes de ler o conteúdo.

--- WebCrawler/Services/CrawlerService.cs
+++ WebCrawler/Services/CrawlerService.cs
@@ -1,7 +1,32 @@
 ﻿
+using WebCrawler.Models;
+
 namespace WebCrawler.Services
 {
     public class CrawlerService
     {
+
+        private readonly HttpClient _httpClient;
+
+        public CrawlerService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<List<ProxyInfo>> ExtractProxiesFromPageAsync(string url)
+        {
+            // 1. Baixar conteúdo HTML
+            var html = await GetHtmlContentAsync(url);
+
+            return new List<ProxyInfo>();
+        }
+
+        private async Task<string> GetHtmlContentAsync(string url)
+        {
+            var response = await _httpClient.GetAsync(url);
+            response.EnsureSuccessStatusCode();
+            return await response.Content.ReadAsStringAsync();
+        }
+
     }
 }
