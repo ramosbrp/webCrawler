f4daa31d3e5a17bd18819c869fa00cbc39f84b1a
Bruno <bruno.rp45@gmail.com>
2025-02-19 11:19:43 -0300

    Atualiza estrutura do projeto e adiciona ProxyInfo
    Removido comentário no `Program.cs` e simplificadas as referências a pastas no `WebCrawler.csproj`. Adicionada a classe `ProxyInfo` no namespace `WebCrawler.Models` para gerenciar informações de proxy, incluindo IP, porta, país e protocolo.

---
+++ WebCrawler/Models/ProxyInfo.cs
@@ -0,0 +1,10 @@
+﻿namespace WebCrawler.Models
+{
+    public class ProxyInfo
+    {
+        public string IpAddress { get; set; }
+        public string Port { get; set; }
+        public string Country { get; set; }
+        public string Protocol { get; set; }
+    }
+}
