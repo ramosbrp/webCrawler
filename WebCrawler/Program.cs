78de3e0e85e1f8f38ae36fd349c766e02a88863c
Bruno <bruno.rp45@gmail.com>
2025-02-19 15:25:42 -0300

    Adicionar registro de horário de início da execução
    Inclui a gravação do horário de início (`startTime`) e uma mensagem no console para informar quando a execução começou. A chamada para `crawlerService.RunAsync()` foi movida para após a gravação do horário de início.

--- WebCrawler/Program.cs
+++ WebCrawler/Program.cs
@@ -6,4 +6,12 @@ var httpClient = new HttpClient();
 // 2. Instanciar Service
 var crawlerService = new CrawlerService(httpClient);
 
+
+// 3. Guardar StartTime
+var startTime = DateTime.Now;
+Console.WriteLine($"Execução iniciada em {startTime}");
+
+// 4. Executar processo
 var proxies = await crawlerService.RunAsync();
+
+// 5. Salvar JSON
