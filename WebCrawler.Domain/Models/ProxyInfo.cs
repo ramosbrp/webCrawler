63d1cfd9565152b73adae827e70fc354c5eb7500
Bruno <bruno.rp45@gmail.com>
2025-02-20 14:11:04 -0300

    Adiciona CrawlerDbContext e atualiza ProxyInfo
    Implementa a classe CrawlerDbContext com DbSets para
    CrawlerExecutions e Proxies. Atualiza ProxyInfo com
    comentário sobre a propriedade Id e mantém as propriedades
    existentes inicializadas no construtor.

--- WebCrawler.Domain/Models/ProxyInfo.cs
+++ WebCrawler.Domain/Models/ProxyInfo.cs
@@ -2,6 +2,7 @@
 {
     public class ProxyInfo(string ipAddress, string? port, string country, string protocol)
     {
+        //public int Id { get; set; } = 0;
         public string IpAddress { get; set; } = ipAddress;
         public string? Port { get; set; } = port;
         public string Country { get; set; } = country;
---
+++ WebCrawler.Infrastructure/Persistence/CrawlerDbContext.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebCrawler.Domain.Models;
+
+namespace WebCrawler.Infrastructure.Persistence
+{
+    public class CrawlerDbContext : DbContext
+    {
+        public CrawlerDbContext(DbContextOptions<CrawlerDbContext> options)
+            : base(options)
+        {
+        }
+
+        // DbSets (tabelas) que iremos mapear
+        public DbSet<CrawlerExecution> CrawlerExecutions { get; set; }
+        public DbSet<ProxyInfo> Proxies { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Configurar a tabela
+            modelBuilder.Entity<CrawlerExecution>(entity =>
+            {
+                entity.ToTable("CrawlerExecutions"); 
+                entity.HasKey(e => e.Id);
+            });
+
+            //modelBuilder.Entity<ProxyInfo>(entity =>
+            //{
+            //    entity.ToTable("Proxies");
+            //    entity.HasKey(p => p.Id);
+            //});
+        }
+    }
+}
