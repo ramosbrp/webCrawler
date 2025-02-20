b487dd1da82f3de45c06ddefa9705ff8ed6b429b
Bruno <bruno.rp45@gmail.com>
2025-02-20 16:08:13 -0300

    Adicionar classe CrawlerRunResult com propriedades e construtor
    Foi criada a classe `CrawlerRunResult` no arquivo `CrawlerRunResult.cs`. Esta classe possui duas propriedades: `Proxies`, uma lista de `ProxyInfo`, e `PagesCount`, um inteiro. Um construtor foi implementado para inicializar essas propriedades.

---
+++ WebCrawler.Application/DTOs/CrawlerRunResult.cs
@@ -0,0 +1,16 @@
+﻿using WebCrawler.Domain.Models;
+
+namespace WebCrawler.Application.DTOs
+{
+    public class CrawlerRunResult
+    {
+        public List<ProxyInfo> Proxies { get; set; }
+        public int PagesCount { get; set; }
+
+        public CrawlerRunResult(List<ProxyInfo> proxies, int pagesCount)
+        {
+            Proxies = proxies;
+            PagesCount = pagesCount;
+        }
+    }
+}
